d0621ab1af2a0bac47fe17f07db976e32265e6c6
ChristianBravoUCOL <christian_bravo@ucol.mx>
2026-09-02 13:09:38 -0600

    Proyecto terminado

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    //Copyright Christian Bravo
+    printf("===============================\n");
+    printf("         ¡Hola, GITHUB!\n");
+    printf("==============================\n\n");
+    printf("Mi nombre es: Christian Bravo\n");
+    printf("Estoy aprendiendo programacion.\n");
+    printf("Este es mi primer programa\n");
+    printf("Publicado en Github\n\n");
+    printf("¡Bienvenido a mi repositorio!\n");
     return 0;
 }
